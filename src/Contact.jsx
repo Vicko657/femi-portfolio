75bf24aa8323d6ace64ff5f2f40b578fdab8b9a6
Vicko657 <victoriaolusegun@gmail.com>
2025-03-17 22:30:51 +0000

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,8 +8,13 @@ export default function Contact() {
       <div className="card-body  d-flex row">
         <div className="col-lg-5 col-12 p-lg-5">
           <h2>Contact Me</h2>
-          <p className="">lifeoffemi@gmail.com</p>
-          <p className="pb-4">+44 7951 147670</p>
+          <p className="">
+            <a className="mb-2" href="mailto:lifeoffemi@gmail.com">
+              lifeoffemi@gmail.com
+            </a>
+            <br />
+            <br /> +44 7951 147670
+          </p>
         </div>
         <div className="col-lg-7 d-lg-block d-none p-lg-5">
           <img className="img-fluid" src="./imgs/femi_contact.png" />
