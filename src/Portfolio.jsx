3b66fdf8f116aaa149e4af4b3cae69fc120a2916
Vicko657 <victoriaolusegun@gmail.com>
2025-03-17 22:50:23 +0000

    Update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -72,7 +72,7 @@ export default function Portfolio() {
             >
               <div className="accordion-body vh-100 d-flex rows">
                 <div className="row">
-                  <div className="col-lg-3 col-12 p-lg-5 pb-4 p-5 ">
+                  <div className="col-lg-3 col-12 p-lg-5 pb-4 pt-3 p-5 ">
                     <p className="portfolio-role">Client</p>
                     <h2 className="text-black mb-3">JBL</h2>
                     <p className="portfolio-role"> Role</p>
